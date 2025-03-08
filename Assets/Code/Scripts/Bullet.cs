4b4e103ff1ba7aff5dcb0fd9ed4233653c8220e9
Eduard Mariniuc <43582902+edwardedi@users.noreply.github.com>
2025-03-08 19:38:12 +0200

    feat: Add plot prefab, one predefined map, two tower types prefab, tower creation, menu, currency logic, tower health and attack, enemy prefab and movement, bullet trajectory and levels logic

---
+++ Assets/Code/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private int bulletDamage = 1;
+
+    private Transform target;
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!target)
+            return;
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * bulletSpeed; //transform translate method
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Health>().TakeDamage(bulletDamage);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Code/Scripts/Health.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField] private int hitPoints = 2;
+    [SerializeField] private int currencyValue = 10;
+
+    private bool isDestroyed = false;
+
+    public void TakeDamage(int damage)
+    {
+        hitPoints -= damage;
+
+        if (hitPoints <= 0 && !isDestroyed)
+        {
+            EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyValue);
+            isDestroyed = true;
+            Destroy(gameObject);
+        }
+    }
+}
